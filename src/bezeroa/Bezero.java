a712fa51eae6d95cf90802f18c48aceb942b2802
Ibai Osa <popbl6a@gmail.com>
2011-06-05 15:46:53 +0200

    Deskargen berrabiarazpena

--- src/bezeroa/Bezero.java
+++ src/bezeroa/Bezero.java
@@ -1,8 +1,12 @@
 package bezeroa;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import org.apache.commons.codec.digest.DigestUtils;
@@ -12,6 +16,8 @@ import erabilgarriak.*;
 public class Bezero {
 	static ArrayList<FileData> files=new ArrayList<FileData>();
 	public static final String ONCOMING_PATH=".";//Konpartitzeko fitxategiak egongo diren helbidea
+	private static final String INCOMING_PATH = null;
+	
 	public static void fileDatakSortu(){
 		File fichero=new File(ONCOMING_PATH);
 		File [] lista;
@@ -53,6 +59,43 @@ public class Bezero {
 	    }
 		}
 	  }
+	
+	//Deskarga berrabiarazi
+
+
+	public static void partakIrakurri() throws Exception{
+		File fichero=new File(INCOMING_PATH);
+		int partKop;
+		ArrayList <Integer> parteak=new ArrayList<Integer>();
+		ArrayList <Integer> faltan=new ArrayList<Integer>();
+		int partea;
+		File [] lista;
+		lista=fichero.listFiles(new Filter(".data"));
+		for (int i=0;i<lista.length;i++){
+		FileInputStream data=new FileInputStream(lista[i]);
+	    DataInputStream in = new DataInputStream(data);
+	    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+	    String name=br.readLine();
+	    Long size=(long)br.read();
+	    String hash=br.readLine();
+	    FileData fd=new FileData();
+	    fd.name=name;
+	    fd.size=size;
+	    fd.hash=hash;
+	    partKop=(int)br.read();
+	    for(int i1=0; i1<partKop; i1++){
+	    	partea=(int)br.read();
+			parteak.add(partea);
+		}
+	    for(int i1=0;i1<partKop;i1++){
+	    	if(!parteak.contains(i1)){
+	    		faltan.add(i1);
+	    	}
+	    }
+	    Deskarga berrabiarazi=new Deskarga(fd,faltan);	    
+		}
+		
+	}
 	
 	
 }
